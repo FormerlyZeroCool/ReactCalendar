f51b5c412e37eeba5eeb159500294524a2fc52b3
andrewrubinstein <andrew.rubinstein93@gmail.com>
2023-04-17 11:16:51 -0400

    Change app to use date object to track current date

--- react-calendar/src/App.js
+++ react-calendar/src/App.js
@@ -18,35 +18,30 @@ class ViewType {
 };
 function App() {
   const first_of_month = get_first_of_month_time();
-  const [week, setWeek] = useState(first_of_month + Math.floor(now().getDate() / 7))
-  const [month, setMonth] = useState(now().getMonth());
-  const [year, setYear] = useState(now().getFullYear());
+  const [date, setDate] = useState(now());
   const [viewType, setViewType] = useState(new ViewType(5, 5));
-  const days = new Array(viewType.total_days).fill(0, 0, viewType.total_days).map((val, i) => i + week * 7);
-  console.log(days)
+  //const days = new Array(viewType.total_days).fill(0, 0, viewType.total_days).map((val, i) => i + week * 7);
+  console.log("hi")
   return (
     <div className="App">
       <header className="App-header">
         <ul>
             <div className="calendar-header">
               <button className="arrow left" onClick={() => {
-                const reset_month = month - 1 < 0;
-                  if(reset_month)
-                    setYear(year - 1);
-                  setMonth(reset_month ? month_lookup.length - 1 : month - 1);
+                  date.setMonth(date.getMonth() - 1);
+                  setDate(new Date(date));
                 }}></button>
               <div
                 role="list"
                 className="list"
                 aria-labelledby="list-heading"
                 >
-                <li>{year}</li>
-                <li>{month_lookup[month]}</li>
+                <li>{date.getFullYear()}</li>
+                <li>{month_lookup[date.getMonth()]}</li>
               </div>
               <button className="arrow right" onClick={() => {
-                  if(month + 1 === month_lookup.length)
-                    setYear(year + 1);
-                  setMonth((month + 1) % month_lookup.length);
+                  date.setMonth(date.getMonth() + 1);
+                  setDate(new Date(date));
                 }}></button>
             </div>
         </ul>
